2cd136bebdaa5a5aa341b7a647639c4e81cd54cf
Andy <andyfartushniy@gmail.com>
2023-12-20 14:35:03 +0200

    Added mongoose

---
+++ controller/contacts-controller.js
@@ -0,0 +1,86 @@
+import HttpError from '../helper/HttpError.js';
+import Contact from '../models/Contact.js';
+import * as contactsService from '../models/contacts.js';
+import { contactAddSchema, contactUpdateSchema } from '../schemas/contact-schema.js';
+
+
+const getAll = async (req, res, next) => {
+    const result = await Contact.find();
+    res.json(result);
+};
+
+    // try {
+    //     const result = await contactsService.listContacts();
+    //     res.json(result);
+    // }
+    // catch (error) {
+    //     next(error);
+    // }
+
+// const getById = async (req, res, next) => {
+//     try {
+//         const { contactId } = req.params;
+//         const result = await contactsService.getContactById(contactId);
+//         if (!result) {
+//             throw HttpError(404)
+//         };
+//         res.json(result);
+//     }
+//     catch (error) {
+//         next(error);
+//     }
+// };
+
+// const addNew = async (req, res, next) => {
+//     try {
+//         const { error } = contactAddSchema.validate(req.body);
+//         if(error) {
+//             throw HttpError(400, error.message);
+//         }
+//         const result = await contactsService.addContact(req.body);
+//         res.status(201).json(result);
+//     }
+//     catch (error) {
+//         next(error);
+//     }
+// };
+
+// const deleteById = async (req, res, next) => {
+//     try {
+//         const { contactId } = req.params;
+//         const result = await contactsService.removeContact(contactId);
+//         if (!result) {
+//             throw HttpError(404)
+//         };
+//         res.json({ message: "contact deleted" });
+//     }
+//     catch (error) {
+//         next(error);
+//     }
+// };
+
+// const updateById = async (req, res, next) => {
+//     try {
+//         const { error } = contactUpdateSchema.validate(req.body);
+//         if (error) {
+//             throw HttpError(400, error.message);
+//         }
+//         const { contactId } = req.params;
+//         const result = await contactsService.updateContact(contactId, req.body);
+//         if (!result) {
+//             throw HttpError(404)
+//         };
+//         res.json(result);
+//     }
+//     catch (error) {
+//         next(error);
+//     }
+// };
+
+export default {
+    getAll,
+    // getById,
+    // addNew,
+    // updateById,
+    // deleteById,
+}
---
+++ routes/api/contacts-router.js
@@ -0,0 +1,19 @@
+import express from 'express';
+
+import contactsController from '../../controller/contacts-controller.js';
+import isEmptyBody from '../../middleware/isEmptyBody.js';
+
+
+const router = express.Router()
+
+router.get('/', contactsController.getAll);
+
+// router.get('/:contactId', contactsController.getById);
+
+// router.post('/', isEmptyBody, contactsController.addNew);
+
+// router.delete('/:contactId', contactsController.deleteById);
+
+// router.put('/:contactId', isEmptyBody, contactsController.updateById);
+
+export default router;
